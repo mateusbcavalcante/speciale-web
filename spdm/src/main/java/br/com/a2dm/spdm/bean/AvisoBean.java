4f9af5db199ccfedfc98ef11f88b6fea68c05658
jpedropmont <jpedropmont@gmail.com>
2023-03-15 14:25:48 -0300

    Inserindo tela de avisos

---
+++ spdm/src/main/java/br/com/a2dm/spdm/bean/AvisoBean.java
@@ -0,0 +1,81 @@
+package br.com.a2dm.spdm.bean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+import br.com.a2dm.brcmn.util.jsf.AbstractBean;
+import br.com.a2dm.brcmn.util.jsf.Variaveis;
+import br.com.a2dm.spdm.config.MenuControl;
+import br.com.a2dm.spdm.entity.Aviso;
+import br.com.a2dm.spdm.service.AvisoService;
+
+@RequestScoped
+@ManagedBean
+public class AvisoBean extends AbstractBean<Aviso, AvisoService>{
+	
+	public AvisoBean()
+	{
+		super(AvisoService.getInstancia());
+		this.ACTION_SEARCH = "aviso";
+		this.pageTitle = "Avisos";
+		
+		MenuControl.ativarMenu("flgMenuMan");
+		MenuControl.ativarSubMenu("flgMenuManCli");
+	}
+	
+	@Override
+	protected void completarInserir() throws Exception
+	{
+		this.getEntity().setAtivo(true);
+	}
+	
+	public void ativar() 
+	{		
+		try
+		{
+			if(this.getEntity() != null)
+			{
+				if(validarAcesso(Variaveis.ACAO_ATIVAR))
+				{
+					AvisoService.getInstancia().ativar(this.getEntity());
+					
+					FacesMessage message = new FacesMessage("Registro ativado com sucesso!");
+					message.setSeverity(FacesMessage.SEVERITY_INFO);
+					FacesContext.getCurrentInstance().addMessage(null, message);
+				}
+			}
+		}
+		catch (Exception e) 
+		{
+			FacesMessage message = new FacesMessage(e.getMessage());
+	        message.setSeverity(FacesMessage.SEVERITY_ERROR);
+	        FacesContext.getCurrentInstance().addMessage(null, message);
+		}		
+	}
+	
+	public void inativar() 
+	{		
+		try
+		{
+			if(this.getEntity() != null)
+			{
+				if(validarAcesso(Variaveis.ACAO_INATIVAR))
+				{
+					AvisoService.getInstancia().inativar(this.getEntity());
+					FacesMessage message = new FacesMessage("Registro inativado com sucesso!");
+					message.setSeverity(FacesMessage.SEVERITY_INFO);
+					FacesContext.getCurrentInstance().addMessage(null, message);
+				}
+			}
+		}
+		catch (Exception e) 
+		{
+			FacesMessage message = new FacesMessage(e.getMessage());
+	        message.setSeverity(FacesMessage.SEVERITY_ERROR);
+	        FacesContext.getCurrentInstance().addMessage(null, message);
+		}		
+	}
+	
+}
